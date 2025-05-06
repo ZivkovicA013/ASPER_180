e45c0e64df40da292bb66085ca9ad43db04ce1e5
Aleksa Zivkovic <azivkovic013@gmail.com>
2025-05-06 17:47:58 +0200

    small ui refactoring

--- app/src/main/java/com/novelic/novelictestapp/radar/RadarScreen.kt
+++ app/src/main/java/com/novelic/novelictestapp/radar/RadarScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -145,6 +146,7 @@ fun RadarFooter() {
                 contentDescription = null,
             )
         }
+        Spacer(Modifier.width(8.dp))
         Box(
             modifier = Modifier
                 .align(Alignment.CenterVertically)
