913720b1ab5eb4350c125dca675081f1ae145b27
Aleksa Zivkovic <azivkovic013@gmail.com>
2025-05-06 17:47:58 +0200

    small ui refactoring

--- app/src/main/java/com/novelic/novelictestapp/radar/RadarScreen.kt
+++ app/src/main/java/com/novelic/novelictestapp/radar/RadarScreen.kt
@@ -16,19 +16,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.novelic.novelictestapp.R
 
 @Composable
 fun RadarScreen() {
     Column(
         modifier = Modifier
-            .padding(16.dp)
+            .padding(start = 16.dp, top = 32.dp, bottom = 32.dp)
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally
     )
@@ -45,30 +48,68 @@ fun RadarScreen() {
 
 @Composable
 fun RadarGrid() {
+
+    val textMeasure = rememberTextMeasurer()
+    val lineColor = MaterialTheme.colorScheme.secondary
+    val gridLabelColor = MaterialTheme.colorScheme.primary
+
     Canvas(modifier = Modifier.fillMaxSize()) {
-        val canvasHeight = size.height
-        val canvasWidth = size.width
-        val gridSpacing = 100f // 100px
-        val stroke = 7f
-        val lineColor = Color.Red
+        // sensor real world ranges
+        val xRange: IntRange = -10..10 // -10m -> 10m
+        val yRange: IntRange = 0..10 // 0m -> 10m
 
-        println("Canvas height : $canvasHeight, Canvas width : $canvasWidth")
 
-        // draw x-axis
-        drawLine(
-            start = Offset(0f, canvasHeight),
-            end = Offset(canvasWidth, canvasHeight),
-            strokeWidth = stroke * 2,
-            color = lineColor
-        )
+        val gridWidth = size.width - 50f
+        val gridHeight = size.height - 50f
 
-        // draw y-axis
-        drawLine(
-            start = Offset(canvasWidth / 2, 0f),
-            end = Offset(canvasWidth / 2, canvasHeight),
-            strokeWidth = stroke * 2,
-            color = lineColor
-        )
+        val xScale = gridWidth / (xRange.last - xRange.first)
+        val yScale = gridHeight / (yRange.last - yRange.first)
+
+        // draw vertical grid lines
+        for (i in xRange step 2) {
+            val x = (i - xRange.first) * xScale
+            drawLine(
+                start = Offset(x, 0f),
+                end = Offset(x, gridHeight),
+                strokeWidth = if (i == 0) 10f else 3f,
+                color = lineColor
+            )
+
+            drawText(
+                textMeasurer = textMeasure,
+                "${i}m",
+                style = TextStyle(
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = gridLabelColor
+                ),
+                topLeft = Offset(x - 20f, gridHeight + 20f)
+            )
+        }
+
+        // draw horizontal grid lines
+        for (i in yRange step 2) {
+            val y = gridHeight - (i - yRange.first) * yScale
+            drawLine(
+                start = Offset(0f, y),
+                end = Offset(gridWidth, y),
+                strokeWidth = if (i == 0) 10f else 3f,
+                color = lineColor
+            )
+
+            if (i != 0) {
+                drawText(
+                    textMeasurer = textMeasure,
+                    "${i}m",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = gridLabelColor
+                    ),
+                    topLeft = Offset(gridWidth / 2 + 10f, y),
+                )
+            }
+        }
     }
 }
 
@@ -87,7 +128,10 @@ fun RadarFooter() {
             Text(
                 text = stringResource(R.string.novelic),
                 textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.displayLarge.copy(fontWeight = FontWeight.ExtraBold)
+                style = MaterialTheme.typography.displayLarge.copy(
+                    fontWeight = FontWeight.ExtraBold,
+                    color = MaterialTheme.colorScheme.tertiary
+                )
             )
         }
         Box(
@@ -111,12 +155,18 @@ fun RadarFooter() {
                 Text(
                     text = stringResource(R.string.asper_180),
                     textAlign = TextAlign.Center,
-                    style = MaterialTheme.typography.displayLarge.copy(fontWeight = FontWeight.ExtraBold)
+                    style = MaterialTheme.typography.displayLarge.copy(
+                        fontWeight = FontWeight.ExtraBold,
+                        color = MaterialTheme.colorScheme.tertiary
+                    )
                 )
                 Text(
                     text = stringResource(R.string.short_range_radar_sensor),
                     textAlign = TextAlign.Start,
-                    style = MaterialTheme.typography.headlineMedium.copy(fontWeight = FontWeight.Bold)
+                    style = MaterialTheme.typography.headlineMedium.copy(
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.tertiary
+                    )
                 )
             }
         }
